0d3523f00d92db1849354c92fc8e936220851d22
William Slettvold Østensen <wos000@post.uit.no>
2019-02-13 19:47:27 +0100

    made the first real commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,57 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div id="menu-view">
+    <div id="name"> <img src="@/assets/logo.svg" alt="Logo"> </div>
+      <div id="menu">
+        <a class="social" href="https://twitter.com/_wost"> <feather type="twitter" /> </a>
+        <a class="social _" href="https://www.linkedin.com/in/william-slettvold-%C3%B8stensen-303a56a1/"> <feather type="linkedin" /> </a>
+        <a class="social _" href="https://github.com/wislos"> <feather type="github" /> </a>
+      </div>
+      <ul id="link-menu">
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
   </div>
+  <div id="second-view">
+    <router-view />
+  </div>
+</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="sass">
+  @import '@/assets/style/all.sass'
+  html
+    background: $bg
+    color: $fg
+    letter-spacing: 0.025rem
+    line-height: 1.5
+    #app
+      width: 75%
+      margin: 1rem auto
+      padding: 1rem
+      #menu-view
+        width: 25%
+        display: inline-block
+        text-align: center
+        #name
+          width: 10rem
+          margin: 0 auto
+        #menu
+          margin-top: 1rem
+          .social
+            padding: 1rem
+          .social svg
+            stroke: $fg
+        #link-menu
+          margin: 1.5rem 0 0 .5rem
+          padding: 0
+          li
+            list-style-type: none
+            a
+              color: $fg
+              text-decoration: none
+              font-size: 1.75rem
+              padding: 1rem 1rem .5rem 1rem
+      #second-view
+        display: inline-block
+        width: 75%
 </style>
